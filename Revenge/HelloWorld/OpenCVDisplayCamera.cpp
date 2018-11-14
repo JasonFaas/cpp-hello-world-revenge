36e70f9c6952f3b7d4c521b57e9123246469cd96
Jason Faas <5844854+JasonFaas@users.noreply.github.com>
2018-11-14 15:23:34 -0800

    Remove extra comments

--- Revenge/HelloWorld/OpenCVDisplayCamera.cpp
+++ Revenge/HelloWorld/OpenCVDisplayCamera.cpp
@@ -11,8 +11,6 @@ int main() {
 	if (!cap.isOpened())  // check if we succeeded
 		return -1;
 	
-
-	//Mat edges;
 	namedWindow("edges", 1);
 	for (;;)
 	{
@@ -20,13 +18,10 @@ int main() {
 
 		Mat frame;
 		cap >> frame; // get a new frame from camera
-		//cvtColor(frame, edges, COLOR_BGR2GRAY);
-		//GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
-		//Canny(edges, edges, 0, 30, 3);
 		imshow("edges", frame);
 		if (waitKey(30) >= 0) break;
 	}
-	// the camera will be deinitialized automatically in VideoCapture destructor
+	// camera will be deinitialized automatically in VideoCapture destructor
 	destroyAllWindows();
 	
 	std::cout << "\n\nReturn to end program" << std::endl;
