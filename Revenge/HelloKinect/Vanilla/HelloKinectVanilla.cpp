db2b43fd8dd34b08a5ac29d341e50b0560fce9ee
Jason Faas <5844854+JasonFaas@users.noreply.github.com>
2018-11-19 20:20:47 -0800

    Display Kinect color frame with OpenCV

--- Revenge/HelloKinect/Vanilla/HelloKinectVanilla.cpp
+++ Revenge/HelloKinect/Vanilla/HelloKinectVanilla.cpp
@@ -9,8 +9,9 @@
 #include <Kinect.h>
 #include <strsafe.h>
 
-// TODO investigate if I need this include
 #include "stdafx.h"
+
+#include <opencv2/opencv.hpp>
 
 int main() {
 	std::cout << "\nHello, World Kinect Vanilla!" << std::endl;
@@ -68,18 +69,40 @@ int main() {
 	std::cout << "Heigh-Width:\t" << nWidth << "--" << nHeight << std::endl;
 	
 
+	cv::Mat bufferMat(nHeight, nWidth, CV_8UC4);
+	cv::Mat colorMat(nHeight / 2, nWidth / 2, CV_8UC4);
+	unsigned int bufferSize = nWidth * nHeight * 4 * sizeof(unsigned char);
+	cv::namedWindow("Kinect_Color");
 
 	//ColorImageFormat imageFormat = ColorImageFormat_None;
 	//hr = pColorFrameReader->AcquireLatestFrame(&pColorFrame);
 	int i = 0;
 	IColorFrame* pColorFrame = NULL;
-	while (i++ < 100) {
+	ColorImageFormat imageFormat = ColorImageFormat_None;
+	while (++i < 100) {
 		pColorFrame = NULL;
 		hr = pColorFrameReader->AcquireLatestFrame(&pColorFrame);
 		std::cout << "HR6." << i << ":\t" << SUCCEEDED(hr) << std::endl;
+		if (SUCCEEDED(hr)) {
+			pColorFrame->get_RawColorImageFormat(&imageFormat);
+			std::cout << "HR7." << i << ":\t" << imageFormat << std::endl;
+			if (imageFormat == ColorImageFormat_Yuy2) {
+				hr = pColorFrame->CopyConvertedFrameDataToArray(bufferSize, reinterpret_cast<BYTE*>(bufferMat.data), ColorImageFormat::ColorImageFormat_Bgra);
+				cv::resize(bufferMat, colorMat, cv::Size(), 0.5, 0.5);
+				cv::imshow("Kinect_Color", colorMat);
 
-		SafeRelease(pColorFrame);
-		Sleep(100);
+				cv::waitKey(30);
+				SafeRelease(pColorFrame);
+			} else {
+				std::cout << "HR8." << i << ":\t" << "ERRROROROROROROR" << std::endl;
+			}
+
+
+
+		}
+
+		Sleep(60);
+
 
 		// TODO Reminder color format may be YUY2 or YUYV
 		//cv::cvtColor( bufferMat, colorMat, CV_YUV2BGR_YUYV );
